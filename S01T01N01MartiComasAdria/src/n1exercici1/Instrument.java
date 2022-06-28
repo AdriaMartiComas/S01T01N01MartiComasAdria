53162ac85243a57da3f6214f18f53f13bd659ef1
adriamarticomas <adriamarticomas@172.20.10.5>
2022-06-28 13:12:47 +0200

    First Commit

---
+++ S01T01N01MartiComasAdria/src/n1exercici1/Instrument.java
@@ -0,0 +1,33 @@
+package n1exercici1;
+
+public abstract class Instrument {
+	private String nom;
+	private int preu;
+	
+	public Instrument(String nom, int preu) {
+		this.nom = nom;
+		this.preu = preu;
+	}
+	
+	public int getPreu() {
+		return preu;
+	}
+
+	public void setPreu(int preu) {
+		this.preu = preu;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public abstract void tocar();
+
+	@Override
+	public String toString() {
+		return "Instrument [nom=" + nom + ", preu=" + preu + "]";
+	}
+	
+	
+
+}
---
+++ S01T01N01MartiComasAdria/src/n1exercici1/InstrumentPercussio.java
@@ -0,0 +1,16 @@
+package n1exercici1;
+
+public class InstrumentPercussio extends Instrument{
+
+	public InstrumentPercussio(String nom, int preu) {
+		super(nom, preu);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void tocar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
